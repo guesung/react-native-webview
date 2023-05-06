59629036b1e7878c16f952922a6c8e5288b6aafa
Kuesung Park <gueit214@naver.com>
2023-05-06 14:47:30 +0900

    feat : add WebView

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,57 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import WebView from 'react-native-webview';
+import {BackHandler, Text, TouchableOpacity, View} from 'react-native';
+import {WebViewNativeEvent} from 'react-native-webview/lib/WebViewTypes';
+import {useNavigation} from '@react-navigation/native';
+import {HeaderBackButton} from '@react-navigation/elements';
 
 const Home = () => {
+  const ref = useRef<WebView>(null);
+  const [navState, setNavState] = useState<WebViewNativeEvent>();
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const canGoBack = navState?.canGoBack;
+
+    const onPress = () => {
+      if (canGoBack) {
+        ref?.current?.goBack();
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    navigation.setOptions({
+      headerLeft: () =>
+        canGoBack ? (
+          <HeaderBackButton onPress={onPress} tintColor="#58595B" />
+        ) : null,
+
+      headerRight: () => (
+        //<-----------headerRight 추가
+        <TouchableOpacity
+          style={{marginRight: 13}}
+          onPress={() => navigation.navigate('Settings')}>
+          <Text>설정</Text>
+        </TouchableOpacity>
+      ),
+    });
+
+    BackHandler.addEventListener('hardwareBackPress', onPress);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onPress);
+    };
+  }, [navState?.canGoBack, navigation]);
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <WebView
+      source={{uri: 'https://naver.com'}}
+      ref={ref}
+      onNavigationStateChange={e => setNavState(e)}
+    />
   );
 };
 export default Home;
